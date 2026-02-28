918cc30bb76140d75a1bef1dfc89c80ee2c00dab
Harman khurana <work.harmankhurana@gmail.com>
2026-02-28 13:46:02 +0530

    Working on user.js Routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 import Router from 'express';
 const userRouter = Router();
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
 import { userModel } from '../database/db';
+
+const saltRounds = 10;
 
 userRouter.post('/signup', async function(req, res){
     const requiredBody = z.object({
@@ -18,16 +21,21 @@ userRouter.post('/signup', async function(req, res){
             message: "Incorrect format used",
             error: parsedDataWithSuccess,
         })
-    }
+    };
+
 
 
     const { firstName, lastName, email, password } = req.body;
+
+
+    const hashedPassword = bcrypt.hash(password, saltRounds);
+    console.log(hashedPassword)
 
     await userModel.create({
         firstName: firstName,
         lastName: lastName,
         email: email,
-        password: password
+        password: hashedPassword
     });
 
     res.json({
