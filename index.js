c76a3e27bb86b93b1e5e8142685a08505642689c
Harman khurana <work.harmankhurana@gmail.com>
2026-02-28 13:23:17 +0530

    Working on user.js Routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import { userRouter } from './routes/user';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/todo', todoRouter);
 
 async function main() {
     await mongoose.connect(MONGO_URL);
@@ -15,6 +18,5 @@ async function main() {
     app.listen(PORT, () => {
         console.log(`Server is running`);
     });
-
 };
 main();
