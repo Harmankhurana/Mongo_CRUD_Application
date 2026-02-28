79b29365c391bf042616a6e2ccbda649f3fa2e97
Harman khurana <work.harmankhurana@gmail.com>
2026-02-28 13:24:39 +0530

    Working on user.js Routes

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import Router from 'express';
+const userRouter = Router();
+
+userRouter.post('/signup', function(req, res){
+
+});
+
+userRouter.post('/signin', function(req, res){
+
+});
+
+export {
+    userRouter,
+}
